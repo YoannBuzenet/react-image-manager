15ee454ca3f5937085f9c9198ee1f99d995f222f
Yoann <ybuzenet@gmail.com>
2023-01-18 08:59:32 +0100

    trying react-select

--- src/components/ImageFieldDropDown/ImageFieldDropDown.jsx
+++ src/components/ImageFieldDropDown/ImageFieldDropDown.jsx
@@ -1,15 +1,11 @@
-import { useCustomizedStyle } from "./imageFieldDropDown.style";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import InputLabel from "@mui/material/InputLabel";
-import FormControl from "@mui/material/FormControl";
+import Select from "react-select";
 
 // REPRENDRE LA YO
 // De quoi générer un dropdown
 // Un array d'objet avec libellé / value
 // une valeur par défaut
 // le nom de la key
-// {type : "dropdown", name : "language", keys : [{name : "", value : "1"},{name : "", value : "2"}],defaultValue : "2", isRequired : true}
+// {type : "dropdown", name : "language", keys : [{label : "", value : "1"},{label : "", value : "2"}],defaultValue : "2", isRequired : true}
 
 const ImageFieldDropDown = ({
   name,
@@ -19,32 +15,20 @@ const ImageFieldDropDown = ({
   defaultValue,
   stateFields,
 }) => {
-  const classes = useCustomizedStyle()();
-
-console.log('Dropdown keys', keys)
+  console.log("Dropdown keys", keys);
 
   return (
-    <div className={classes.imageFieldContainer}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">
-          {name}
-          {isRequired && " *"}
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={stateFields?.[name].value}
-          label={name}
-          onChange={(e) => handleChange(e, name)}
-          defaultValue={defaultValue}
-        >
-          {keys.map((oneKey, index) => (
-            <MenuItem value={oneKey.value} key={index} >
-              {oneKey.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+    <div className={"imageFieldContainer"}>
+      <p>
+        {name}
+        {isRequired && " *"}
+      </p>
+      <Select
+        defaultValue={defaultValue}
+        onChange={(e) => handleChange(e, name)}
+        options={keys}
+        value={stateFields?.[name].value}
+      />
     </div>
   );
 };
